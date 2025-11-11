e260e3eb74f843c2d67ed76e5e0bab291b77f905
mayanknetam1234 <netammayank547@gmail.com>
2025-11-11 09:29:28 +0530

    Frontend commited

--- vendorManagement/vendorManagement/src/main/java/com/MoveInSync/vendorManagement/enumClass/DocumentType.java
+++ vendorManagement/vendorManagement/src/main/java/com/MoveInSync/vendorManagement/enumClass/DocumentType.java
@@ -3,5 +3,20 @@ package com.MoveInSync.vendorManagement.enumClass;
 public enum DocumentType {
     LICENSE,
     RC,
-    INSURANCE
+    INSURANCE,
+    PERMIT,
+    FITNESS,
+    PUC,
+    FASTAG,
+    INSPECTION,
+    AADHAAR,
+    PAN,
+    BADGE,
+    POLICE_VERIFICATION,
+    AGREEMENT,
+    CONTRACT,
+    TAX_CERTIFICATE,
+    INVOICE,
+    OTHER
+
 }
